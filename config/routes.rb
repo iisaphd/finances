12e4228720c895f2dda0050e8738528d8dae2de9
Matt McLaurine <matthew.mclaurine@gmail.com>
2015-12-18 20:30:40 +0000

    Minor updates

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
 
   get 'finance/show'
 
-  get 'finance/clear'
-
-  get 'finance/show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
