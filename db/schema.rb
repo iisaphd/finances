172ad59b68c9e690336a3ed506412f6416e185c7
Matt McLaurine <matt@caitlain.com>
2016-01-05 12:58:47 -0500

    updated schema and cronjob

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151220181102) do
+ActiveRecord::Schema.define(version: 20151230182051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -127,6 +127,7 @@ ActiveRecord::Schema.define(version: 20151220181102) do
     t.float    "changerate"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.float    "pctnw"
   end
 
   create_table "notices", force: :cascade do |t|
